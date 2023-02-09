f77e4a1032ec27b32e7b8ca46b9d577a9efc481e
Quackat <55213168+ThangLC304@users.noreply.github.com>
2023-02-09 15:49:10 +0800

    Create yolo_download.bat

---
+++ APP/yolo_download.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set repo=https://github.com/WongKinYiu/yolov7.git
+set target_dir=%cd%\bin\yolov7
+
+if not exist "%target_dir%" (
+  echo Cloning YOLOv7 repository...
+  git clone %repo% %target_dir%
+) else (
+  echo YOLOv7 repository already exists.
+)
+
+echo Done.
