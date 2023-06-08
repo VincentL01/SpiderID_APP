5942b78f88eb029053ca668109cba68ff2f2651a
Quackat <55213168+ThangLC304@users.noreply.github.com>
2023-06-08 13:54:13 +0800

    small change

--- APP/run.bat
+++ APP/run.bat
@@ -1,13 +1,66 @@
 @echo off
-set "venv_name=test"
-if exist "%USERPROFILE%\anaconda3" (
-  set "conda_path=%USERPROFILE%\anaconda3"
+setlocal enabledelayedexpansion
+
+set "venv_name=spiderid_env"
+
+REM set APP_DIR to directory of this file
+set "APP_DIR=%~dp0"
+
+if "%OS%"=="Windows_NT" (
+  set "bat_found=0"
+  set "env_found=0"
+
+  for %%p in (
+    "C:\ProgramData\miniconda3"
+    "C:\ProgramData\Anaconda3"
+    "C:\ProgramData\.conda"
+    "%UserProfile%\miniconda3"
+    "%UserProfile%\.conda"
+    "%UserProfile%\Anaconda3"
+  ) do (
+      if exist "%%~p\envs\%venv_name%" (
+          set "env_path=%%~p\envs\%venv_name%"
+          set "env_found=1"
+      )
+      if exist "%%~p\Scripts\activate.bat" (
+          set "bat_path=%%~p\Scripts\activate.bat"
+          set "bat_found=1"
+      )
+  )
+
+  if !bat_found!==0 (
+      echo activate.bat not found in any of the specified paths.
+      echo Please rerun setup.bat to install Miniconda3.
+      pause
+      exit
+  )
+
+  if !env_found!==0 (
+      echo Environment %venv_name% not found in any of the specified paths.
+      echo Please rerun setup.bat to install Miniconda3.
+      pause
+      exit 
+  )
+
+  set "activate_cmd=!bat_path! !env_path!"
+
+  echo %activate_cmd%
+
 ) else (
-  set "conda_path=%USERPROFILE%\miniconda3"
+  set "conda_path=$HOME/miniconda3"
+  set "activate_cmd=source $conda_path/bin/activate %venv_name%"
 )
+
+echo %activate_cmd%
+
+REM activate TAN environment
 echo Activating virtual environment...
-echo Locating virtual environment in %conda_path%\envs\%venv_name%
-call %conda_path%\Scripts\activate.bat %conda_path%\envs\%venv_name%
-echo Run Program
-python main.py
-pause
+call %activate_cmd%
+
+echo Virtual environment (%venv_name%) activated
+
+echo Running program...
+"%CONDA_PREFIX%\python.exe" "%APP_DIR%main.py"
+
+if "%OS%"=="Windows_NT" pause
+else read -p "Press [Enter] key to continue..."
--- APP/setup.bat
+++ APP/setup.bat
@@ -1,66 +1,93 @@
 @echo off
-set miniconda_url="https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-set miniconda_installer="%~dp0Miniconda3-latest-Windows-x86_64.exe"
-set python_version="3.9.13"
-set venv_name="spiderid_venv"
+setlocal enabledelayedexpansion
 
-set spiderid_dir=C:\SpiderID
+set "python_version=3.9.13"
+set "venv_name=spiderid_env"
 
-echo The SpiderID_APP application would be installed by default at %APP_INSTALL_PATH%
-set /p change_path=Do you want to change? (Y/N)
+REM set APP_DIR to current directory
+set "APP_DIR=%~dp0"
 
-if /i "%change_path%"=="Y" (
-    set /p spiderid_dir=Enter the new installation path:
-)
-
-echo The SpiderID_APP application will be installed at %spiderid_dir%
-
-if not exist "%UserProfile%\miniconda3" (
-  if not exist "%UserProfile%\anaconda3" (
-    echo Downloading Miniconda...
-    powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%miniconda_url%', '%miniconda_installer%')"
-
-    echo Installing Miniconda...
-    start /wait "" "%miniconda_installer%" /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
-  )
-)
-
-if exist "%UserProfile%\anaconda3" (
-  set "conda_path=%UserProfile%\anaconda3\Scripts"
+if "%OS%"=="Windows_NT" (
+  set "miniconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+  set "miniconda_installer=%~dp0Miniconda3-latest-Windows-x86_64.exe"
 ) else (
-  set "conda_path=%UserProfile%\miniconda3\Scripts"
+  set "miniconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+  set "miniconda_installer=%~dp0Miniconda3-latest-MacOSX-x86_64.sh"
 )
 
-REM Task 1
-set "venv_exists=0"
-for /f "tokens=*" %%a in ('%conda_path%\conda env list') do (
-  for /f "tokens=1" %%b in ("%%a") do (
-    if /i "%%b"=="%venv_name%" (
-      set "venv_exists=1"
-    )
-  )
-)
 
-if %venv_exists%==1 (
-  echo Virtual environment %venv_name% already exists. Checking Python version...
-  for /f "tokens=*" %%a in ('%conda_path%\conda list --revisions -n %venv_name%') do (
-    for /f "tokens=1-3" %%b in ("%%a") do (
-      if /i "%%b"=="python" (
-        if not "%%c"=="%python_version%" (
-          echo Python version mismatch. Creating alternate environment...
-          set "venv_name=spiderid_venv_alt"
-          call %conda_path%\conda create -n %venv_name% python=%python_version% -y
-        )
+REM if not exists anaconda or miniconda, install
+if "%OS%"=="Windows_NT" (
+  set "bat_found=0"
+
+  for %%p in (
+    "C:\ProgramData\miniconda3"
+    "C:\ProgramData\Anaconda3"
+    "C:\ProgramData\.conda"
+    "%UserProfile%\miniconda3"
+    "%UserProfile%\.conda"
+    "%UserProfile%\Anaconda3"
+  ) do (
+      if exist "%%~p\Scripts\activate.bat" (
+          set "conda_path=%%~p"
+          set "bat_found=1"
       )
+  )
+
+  if !bat_found!==0 (
+      echo Downloading Miniconda...
+      powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%miniconda_url%', '%miniconda_installer%')"
+
+      echo Installing Miniconda...
+      start /wait "" "%miniconda_installer%" /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
+
+      set "conda_path=%UserProfile%\miniconda3"
     )
+  ) else (
+      echo Anaconda or Miniconda already installed
   )
 ) else (
-  echo Creating virtual environment with Python %python_version%...
-  call %conda_path%\conda create -n %venv_name% python=%python_version% -y
+  if not exist "$HOME/miniconda3" (
+    if not exist "$HOME/anaconda3" (
+      echo Downloading Miniconda...
+      curl %miniconda_url% -o %miniconda_installer%
+
+      echo Installing Miniconda...
+      chmod +x %miniconda_installer%
+      bash %miniconda_installer% -b -p $HOME/miniconda3
+
+      set "conda_path=$HOME/miniconda3"
+    ) else (
+      set "conda_path=$HOME/anaconda3"
+    )
+  ) else (
+      echo Miniconda already installed
+
+      set "conda_path=$HOME/miniconda3"
+  )
 )
 
-echo Activating virtual environment...
-call %conda_path%\activate.bat %conda_path%\..\envs\%venv_name%
+if exist "%conda_path%\envs\%venv_name%" (
+    echo %venv_name% already exists
+) else (
+    echo %venv_name% not found
+    echo Creating virtual environment with Python %python_version%...
+    if "%OS%"=="Windows_NT" (
+        call %conda_path%\Scripts\conda create -n %venv_name% python==%python_version% -y
+        set "activate_cmd=call %conda_path%\Scripts\activate.bat %venv_name%"
+    ) else (
+        $conda_path/bin/conda create -n %venv_name% python=%python_version% -y
+        set "activate_cmd=source $conda_path/bin/activate %venv_name%"
+    )
+)
+
+
+echo Activating virtual environment... at %conda_path%\envs\%venv_name%
+%activate_cmd%
+
+echo Installing TAN requirements
+cd %APP_DIR%
+pip install -r requirements.txt
 
 REM Check if Git is installed
 where git >nul 2>nul
@@ -73,34 +100,13 @@ if errorlevel 1 (
 
 echo Virtual environment setup complete. You can now use Git and other tools within this environment.
 
-REM Task 2
-if exist "%spiderid_dir%" (
-  set /p "user_input=The folder C:\SpiderID already exists. Do you want to overwrite it? (Y/N): "
-  if /i "%user_input%"=="Y" (
-    echo Removing existing SpiderID folder...
-    rmdir /s /q "%spiderid_dir%"
-  ) else (
-    goto CloneYOLOv7
-  )
-)
-
-echo Make folder for spiderid at %spiderid_dir%
-mkdir "%spiderid_dir%"
-
-echo Clone SpiderID repository from https://github.com/ThangLC304/SpiderID_APP to spiderid_dir
-set "spider_repo=https://github.com/ThangLC304/SpiderID_APP"
-git clone %spider_repo% "%spiderid_dir%"
-
-:CloneYOLOv7
-echo Change directory to %spiderid_dir%
-cd "%spiderid_dir%"
 
 set "yolo_repo=https://github.com/WongKinYiu/yolov7.git"
-set "target_dir=%spiderid_dir%\bin\yolov7"
+set "target_dir=%APP_DIR%\bin\yolov7"
 
 REM Task 3
 if exist "%target_dir%" (
-  set /p "user_input=The folder %spiderid_dir%\bin\yolov7 already exists. Do you want to overwrite it? (Y/N): "
+  set /p "user_input=The folder %APP_DIR%\bin\yolov7 already exists. Do you want to overwrite it? (Y/N): "
   if /i "%user_input%"=="Y" (
     echo Removing existing YOLOv7 folder...
     rmdir /s /q "%target_dir%"
@@ -118,30 +124,6 @@ pip install -r requirements.txt"
 
 echo Copying detect.py to %target_dir% folder...
 copy detect.py "%target_dir%" /y
-
-REM Task 4
-echo Creating run.bat file...
-if exist run.bat (
-  del run.bat
-)
-
-(
-Echo @echo off
-Echo set "venv_name=%venv_name%"
-Echo if exist "%%USERPROFILE%%\anaconda3" ^(
-Echo   set "conda_path=%%USERPROFILE%%\anaconda3"
-Echo ^) else ^(
-Echo   set "conda_path=%%USERPROFILE%%\miniconda3"
-Echo ^)
-Echo echo Activating virtual environment...
-Echo echo Locating virtual environment in %%conda_path%%\envs\%%venv_name%%
-Echo call %%conda_path%%\Scripts\activate.bat %%conda_path%%\envs\%%venv_name%%
-Echo echo Run Program
-Echo python main.py
-Echo pause
-) > run.bat
-
-echo run.bat created.
 
 echo =============================
 echo    INSTALLATION COMPLETED
